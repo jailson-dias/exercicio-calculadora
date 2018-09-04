fe667c7de4c90c23fc9fc0aeef95aae1261c91da
nuneshere <mngs@cin.ufpe.br>
2018-09-04 12:30:30 -0300

    adicionando ações da calculadora

--- calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
@@ -1,13 +1,70 @@
 package br.ufpe.cin.if710.calculadora
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.max
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // recuperando os dados da configuração anterior
+        text_calc.setText(savedInstanceState?.getString("edit"))
+        text_info.setText(savedInstanceState?.getString("result"))
+
+        // colocando para quando digitar nos botões de números adicionar o número no text_calc
+        btn_0.setOnClickListener { text_calc.text.append('0') }
+        btn_1.setOnClickListener { text_calc.text.append('1') }
+        btn_2.setOnClickListener { text_calc.text.append('2') }
+        btn_3.setOnClickListener { text_calc.text.append('3') }
+        btn_4.setOnClickListener { text_calc.text.append('4') }
+        btn_5.setOnClickListener { text_calc.text.append('5') }
+        btn_6.setOnClickListener { text_calc.text.append('6') }
+        btn_7.setOnClickListener { text_calc.text.append('7') }
+        btn_8.setOnClickListener { text_calc.text.append('8') }
+        btn_9.setOnClickListener { text_calc.text.append('9') }
+
+        // colocando para quando clicar nos botões de expressões adiciono-las no text_cal
+        btn_Divide.setOnClickListener { text_calc.text.append('/') }
+        btn_Multiply.setOnClickListener { text_calc.text.append('*') }
+        btn_Subtract.setOnClickListener { text_calc.text.append('-') }
+        btn_Add.setOnClickListener { text_calc.text.append('+') }
+        btn_Dot.setOnClickListener { text_calc.text.append('.') }
+        btn_Power.setOnClickListener { text_calc.text.append('^') }
+        btn_LParen.setOnClickListener { text_calc.text.append('(') }
+        btn_RParen.setOnClickListener { text_calc.text.append(')') }
+
+        btn_Equal.setOnClickListener {
+            try {
+                // colocando para resolver a expressão quando clicar em igual
+                text_info.text = eval(text_calc.text.toString()).toString()
+            } catch (err: Exception) {
+
+                // Exibir toast quando a expressão inserida apresenta algum erro
+                Toast.makeText(applicationContext, "Erro na expressão -> ${err.message}", Toast.LENGTH_LONG).show()
+
+            }
+        }
+        btn_Clear.setOnClickListener {
+            // remove o último caracter do text_calc
+            text_calc.setText(text_calc.text.subSequence(0,max(text_calc.text.lastIndex, 0)))
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+
+        // salvando os dados do text_calc e text_info para quando mudar as configurações
+        outState?.putString("edit", text_calc.text.toString())
+        outState?.putString("result", text_info.text.toString())
     }
 
     //Como usar a função:
